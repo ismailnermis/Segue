f9a771e28548009e8978a9bb7cf4f06b1b10a0b1
İsmail Nermiş <39004389+ismailnermis@users.noreply.github.com>
2023-09-19 17:16:21 +0300

    Initial Commit

---
+++ Homework4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework4
+//
+//  Created by İsmail Nermiş on 19.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
